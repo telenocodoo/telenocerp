6d35eed634da55dab73a08ca01c0081d6f208b9a
TeleNoc Team <45532030+telenocodoo@users.noreply.github.com>
2020-04-09 15:41:54 +0300

    Account_partner

---
+++ hr_payroll_updates/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hr payroll updates",
+    'summary': """
+        Hr payroll updates""",
+    'description': """
+        Hr payroll updates
+    """,
+    'author': "Magdy, helcon",
+    'website': "https://telenoc.org",
+    'category': 'hr',
+    'version': '0.1',
+    'depends': ['hr_payroll_account'],
+    'data': [
+        # 'views/res_users.xml',
+    ],
+}
