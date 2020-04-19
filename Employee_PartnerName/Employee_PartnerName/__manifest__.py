b706da66860afa5f0f68ea83068033bb3ef7ef71
TeleNoc Team <45532030+telenocodoo@users.noreply.github.com>
2020-04-19 15:35:40 +0300

    Add files via upload
    To show the partner name for employee in employee list view report (Employees module)

---
+++ Employee_PartnerName/Employee_PartnerName/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee_PartnerName",
+    'summary': """
+        add Employee Partner Name in tree view""",
+    'description': """
+        Employee_PartnerName
+    """,
+    'author': "Magdy, helcon",
+    'website': "https://telenoc.org",
+    'category': 'hr',
+    'version': '0.1',
+    'depends': ['hr'],
+    'data': [
+        'views/employee_view.xml',
+    ],
+}
