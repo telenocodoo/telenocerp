57bbba7b8918d4a0c5026f0d58d40d6e0a861d10
TeleNoc Team <45532030+telenocodoo@users.noreply.github.com>
2020-04-19 15:49:54 +0300

    Add files via upload
    Its showing Contact type (vendor / customer / employee) in contact form and contact list view report (Contact module)

---
+++ Contact_ContactType/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contact_ContactType",
+    'summary': """
+        add ContactType in tree view of Contacts""",
+    'description': """
+        Contact_ContactType
+    """,
+    'author': "Magdy, helcon",
+    'website': "https://telenoc.org",
+    'category': 'contacts',
+    'version': '0.1',
+    'depends': ['contacts'],
+    'data': [
+        'views/contact_view.xml',
+    ],
+}
