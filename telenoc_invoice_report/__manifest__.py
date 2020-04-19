d0e784857ca19ff8da9c16a7a5b064cb5fadd63c
TeleNoc Team <45532030+telenocodoo@users.noreply.github.com>
2020-04-19 15:59:26 +0300

    Add files via upload
    this module is for customized invoice for TeleNoc

---
+++ telenoc_invoice_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Telenoc Invoice",
+    'summary': """
+        Telenoc Invoice""",
+    'description': """
+        Telenoc Invoice
+    """,
+    'author': "Magdy, helcon",
+    'website': "https://telenoc.org",
+    'category': 'Account',
+    'version': '0.1',
+    'depends': ['web'],
+    'data': [
+        # 'views/account_invoice.xml',
+        # 'report/sales_report.xml',
+        'report/invoice_report.xml',
+    ],
+}
