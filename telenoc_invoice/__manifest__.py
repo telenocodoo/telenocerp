ac808c451b2965aa1a529fac8a65b641fa4255fa
magdii <magdysala@gmail.com>
2020-04-27 19:21:46 +0300

    telenoc invoice

---
+++ telenoc_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Telenoc Invoice report",
+    'summary': """
+        Telenoc Invoice""",
+    'description': """
+        Telenoc Invoice
+    """,
+    'author': "Magdy, helcon",
+    'website': "https://telenoc.org",
+    'category': 'Account',
+    'version': '0.1',
+    'depends': ['web'],
+    'data': [
+        'report/invoice_report.xml',
+    ],
+}
